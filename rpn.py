6d08c7730f0ebba66bcb322e5e6d2c808553b57f
taswartz <taswartz@umich.edu>
2018-11-05 10:19:46 -0500

    Added travis file

--- rpn.py
+++ rpn.py
@@ -1,33 +1,34 @@
-#!/usr/bin/env python3
-
-def calculate(arg):
-    stack = []
-
-    tokens = arg.split()
-
-    for token in tokens:
-    	try: 
-    		stack.append(int(token))
-    	except ValueError:
-    		val1 = stack.pop()
-    		val2 = stack.pop()
-    		if token == '+':
-    			result = val1 + val2
-    		elif token == '-':
-    			result = val2 - val1
-    		stack.append(result)
+import operator
 
 
-    if len(stack) > 1:
-    	raise ValueError("Too many arguments on the stack")		
-    return stack[0]
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
-    	try:
-        	result = calculate(input("rpn calc> "))
-        	print(result)
-        except ValueError:
-        	pass
-if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
+if __name__ == '__main__':
     main()
